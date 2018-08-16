17800da5a56b7bfdafd6e96065c61283c476cbd0
yenisbel <yenisbelv@gmail.com>
2018-08-16 11:12:03 -0700

    Update reverse_string_example.rb

--- reverse_string_example.rb
+++ reverse_string_example.rb
@@ -1,9 +1,6 @@
-Write a method that will take a string as input, and return a new
-# string with the same letters in reverse order.
-#
+#method that will take a string as input, and return a new
+#string with the same letters in reverse order.
 # Don't use String's reverse method; that would be too simple.
-#
-# Difficulty: easy.
 
 def reverse(string)
   reversed = []
